7c239e25339f8bdabc1e1d82972fa24f5e027af8
sanoyo <sa00380304yo@gmail.com>
2021-12-17 08:08:39 +0900

    Add gateway

---
+++ gateway/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	pb "github.com/sanoyo/mini-lens-go/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	endpoint   = "localhost:8080"
+	listenAddr = ":3000"
+)
+
+func init() {
+	ep := os.Getenv("ENDPOINT")
+	if ep != "" {
+		endpoint = ep
+	}
+}
+
+func newGateway(ctx context.Context, opts ...runtime.ServeMuxOption) (http.Handler, error) {
+	mux := runtime.NewServeMux(opts...)
+	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
+
+	conn, err := grpc.Dial(endpoint, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+	err = pb.RegisterHealthServiceHandler(ctx, mux, conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return mux, nil
+}
+
+// Run starts a HTTP server and blocks forever if successful.
+func Run(address string, opts ...runtime.ServeMuxOption) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	gw, err := newGateway(ctx, opts...)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(address, gw)
+}
+
+func main() {
+	fmt.Println("Listen Address:", listenAddr)
+	if err := Run(listenAddr); err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/sanoyo/mini-lens-go
 go 1.16
 
 require (
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
 	k8s.io/apimachinery v0.22.4 // indirect
 	k8s.io/client-go v0.22.4 // indirect
 )
